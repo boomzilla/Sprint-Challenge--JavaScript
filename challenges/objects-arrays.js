0d26a4178b5d083e0238276995feb0566b0f5543
Ian Ruotsala <ian.ruotsala@gmail.com>
2019-04-06 09:32:23 -0700

    logging dinosaurs

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -24,7 +24,7 @@ const stegosaurus = {
   "period": "Late Jurassic"
 }
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-const tyrannosaurus = {
+const velociraptor = {
   "name": "velociraptor",
   "diet": "carnivorous",
   "weight": "15kg",
@@ -34,16 +34,16 @@ const tyrannosaurus = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
